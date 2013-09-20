0ac71384dd64c57a9bd29c22faf3f1a54b7b0e75
Steven E. Newton <steven@janrain.com>
2013-09-19 18:02:03 -0700

    new cool and more 'functional' solution for making change

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -16,9 +16,9 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 || r == 0 || c == r)  {
-      return 1;
+       1
     } else {
-      return pascal(c - 1, r - 1) + pascal(c, r - 1);
+       pascal(c - 1, r - 1) + pascal(c, r - 1);
     }
   }
 
@@ -28,44 +28,36 @@ object Main {
   def balance(chars: List[Char]): Boolean = {
     def doBalance(chars: List[Char], depth: Int, isBalanced: Boolean): Boolean = {
       if (chars.isEmpty)
-        return (depth == 0) && isBalanced
-
-      var newDepth = depth
-      var currentBalance = isBalanced
-      if (chars.head == '(')  {
-        newDepth = depth + 1
+         (depth == 0) && isBalanced
+      else {
+        var newDepth = depth
+        var currentBalance = isBalanced
+        if (chars.head == '(')  {
+          newDepth = depth + 1
+        }
+        if (chars.head == ')') {
+          newDepth = depth - 1
+          // but if we were balanced (depth == 0) we're now unbalanced
+          // and we'll never be balanced again
+          if (newDepth < 0)
+            currentBalance = false;
+        }
+        doBalance(chars.tail, newDepth, currentBalance)
       }
-      if (chars.head == ')') {
-        newDepth = depth - 1
-        // but if we were balanced (depth == 0) we're now unbalanced
-        // and we'll never be balanced again
-        if (newDepth < 0)
-          currentBalance = false;
-      }
-      doBalance(chars.tail, newDepth, currentBalance)
     }
 
-    return doBalance(chars, 0, true)
+     doBalance(chars, 0, true)
   }
 
   /**
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    if (money == 0 || coins.isEmpty)
-      return 0
-
-    var waysToMakeChange = 0
-    if (coins.head == 0)
-      return countChange(money, coins.tail)
-
-    if (money % coins.head == 0)
-      waysToMakeChange = waysToMakeChange + 1
-
-    for(goesInto <- 0 to (money / coins.head)) {
-      waysToMakeChange = waysToMakeChange + countChange(money - (coins.head * goesInto), coins.tail)
-    }
-
-    return waysToMakeChange
+    if (money == 0)
+      1
+    else if (coins.isEmpty || money < 0)
+      0
+    else
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
